927e55666afaf41d6850c68a3d0942d2421b71d4
Hasibul-git <93538796+Hasibul-git@users.noreply.github.com>
2021-12-17 09:22:47 +0800

    //font Set the fonts Implementing Splash Screen

--- app/src/main/java/com/e/immagegallery/MainActivity.java
+++ app/src/main/java/com/e/immagegallery/MainActivity.java
@@ -2,13 +2,30 @@ package com.e.immagegallery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.e.immagegallery.Activity.HomeActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
     }
 }
