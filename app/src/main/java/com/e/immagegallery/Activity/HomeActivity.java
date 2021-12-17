7b0d0e9bb37d6b7fab86ef1ed7418708a80cad61
Hasibul-git <93538796+Hasibul-git@users.noreply.github.com>
2021-12-17 21:10:21 +0800

    //font Set the fonts Implementing Splash Screen

--- app/src/main/java/com/e/immagegallery/Activity/HomeActivity.java
+++ app/src/main/java/com/e/immagegallery/Activity/HomeActivity.java
@@ -1,16 +1,58 @@
 package com.e.immagegallery.Activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.e.immagegallery.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private ImageView imageView;
+    private Bitmap bitmap;
+    private Button btnShare, btnEdit;
+    private Uri imageUri = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        imageView = findViewById(R.id.imageView);
+        btnShare = findViewById(R.id.btnShare);
+        btnEdit = findViewById(R.id.btnEdit);
+
+        uploadImage();
+    }
+
+    private void uploadImage(){
+        Intent gallery = new Intent();
+        gallery.setType("image/*");
+        gallery.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(gallery, "Sellect Photo"), 100);
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(requestCode == 100 && resultCode == RESULT_OK){
+            imageUri = data.getData();
+            try{
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),imageUri);
+                imageView.setImageBitmap(bitmap);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
     }
 }
